f06eecc6f5fc5dd21e404e2499f629c259a78d5b
Mohsin Azam <mohsin.afridi91@gmail.com>
2024-05-14 23:27:25 +0400

    inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'doctors',
+    loadChildren: () =>
+      import('./modules/doctor/doctor.module').then((m) => m.DoctorModule),
+  },
+  /*
+  { path: 'login', component: LoginComponent },  
+  { path: '**', component: PageNotFoundComponent }
+  */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/doctor/doctor.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DoctorRoutingModule } from './doctor-routing.module';
+
+@NgModule({
+  imports: [CommonModule, DoctorRoutingModule],
+  declarations: [],
+})
+export class DoctorModule {}
